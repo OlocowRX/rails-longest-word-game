ace71678e5e6fe38a6d70989dadc64fdac175e01
gabriel tavares <gabriel29t@gmail.com>
2022-02-21 17:47:04 -0300

    Challenge solved

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,4 @@
 require 'open-uri'
-require 'json'
-
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
@@ -11,11 +9,23 @@ class GamesController < ApplicationController
     @guess = params[:word].upcase
     @start_time = Time.parse(params[:start_time])
     @end_time = Time.now
-    # ------------------------------------|
     @time_taken = @end_time - @start_time
+    # ------------------------------------|
     @points = points(@guess, @time_taken)
     # ------------------------------------|
     compute_score(@guess, @letters, @points)
+  end
+
+  def compute_score(guess, grid, points)
+    if included?(guess, grid)
+      if english_word(guess)
+        @result = "Congratulations! #{guess} is a valid English word! And your score was #{points}"
+      else
+        @result = "Sorry but #{guess} does not seem to be a valid English word..."
+      end
+    else
+      @result = "Sorry but #{guess}, can't be build out of #{grid.join(',')}"
+    end
   end
 
   def included?(guess, grid)
@@ -30,17 +40,5 @@ class GamesController < ApplicationController
 
   def points(guess, time_taken)
     time_taken > 60.0 ? 0 : guess.size * (1.0 - time_taken / 60.0)
-  end
-
-  def compute_score(guess, grid, points)
-    if included?(guess, grid)
-      if english_word(guess)
-        @result = "Congratulations! #{guess} is a valid English word! And your score was #{points}"
-      else
-        @result = "Sorry but #{guess} does not seem to be a valid English word..."
-      end
-    else
-      @result = "Sorry but #{guess}, can't be build out of #{grid.join(',')}"
-    end
   end
 end
